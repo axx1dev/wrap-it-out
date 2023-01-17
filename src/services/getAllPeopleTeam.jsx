121b3b68d4e68934e54e991902d26b52f7427fbb
axl1dev <axelpalacioos@gmail.com>
2023-01-17 03:15:12 -0600

    fix: catch error when api shutdown return array const

--- src/services/getAllPeopleTeam.jsx
+++ src/services/getAllPeopleTeam.jsx
@@ -1,12 +1,17 @@
 import axios from 'axios';
+import { teamData } from "../data/index";
 const baseUrl = 'http://localhost:3001/data'
 
 const getAllPeopleTeam = async () => {
   try {
     const result = await axios.get(baseUrl)
+    if (result.status !== 200) {
+      throw new Error('error connection')
+    }
     return result.data
   } catch (error) {
-    throw error
+    //console.error(error)
+    return teamData
   }
   
 }
